e4e65d4decd1be9b04da82c5cf2ed5853d8512e9
Santiago Duque <duquesdr@gmail.com>
2018-04-03 09:56:28 +0200

    Remove comments

--- Composite/Client.cs
+++ Composite/Client.cs
@@ -6,22 +6,6 @@ namespace Composite
     {
         public static void Example()
         {
-            //IFileReader fileReaderCrypto = new FileReader();
-            //fileReaderCrypto.Add(new CrytpoReader());
-
-
-            //string text = fileReaderCrypto.Read();
-
-
-            //IFileReader fileReaderCrytpoZip = new FileReader();
-            //var crytpo = new Crytpo();
-            //crytpo.Add(new Zipper());
-            //fileReaderCrytpoZip.Add();
-
-
-            //string text2 = fileReaderCrytpoZip.Read();
-
-
             IVisualComponent screen = new VisualContainer();
 
             screen.Add(new Icon());
